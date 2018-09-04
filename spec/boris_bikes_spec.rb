bcde0e7aa14cc3005c6a54ed7af63a470b891cf2
Emma Albury <emma_albury@hotmail.com>
2018-09-04 15:25:05 +0100

    first test

---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,18 @@
+require "boris_bikes"
+
+describe DockingStation do
+end
+
+
+
+# # spec/string_calculator_spec.rb
+# describe StringCalculator do
+#
+#   describe ".add" do
+#     context "given an empty string" do
+#       it "returns zero" do
+#         expect(StringCalculator.add("")).to eql(0)
+#       end
+#     end
+#   end
+# end
